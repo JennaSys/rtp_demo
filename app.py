e8504a4b6789849e2ddcdf8dcbe6857425b1e245
John Sheehan <jennasyseng@gmail.com>
2021-01-22 02:46:25 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from pyreact import useState, render, createElement as el
+
+
+def ListItems(props):
+    items = props['items']
+    return [el('li', {'key': item}, item) for item in items]
+
+
+def App():
+    newItem, setNewItem = useState("")
+    items, setItems = useState([])
+
+    def handleSubmit(event):
+        event.preventDefault()
+        # setItems(items.__add__(newItem))
+        setItems(items + [newItem])  # __:opov
+        setNewItem("")
+
+    def handleChange(event):
+        target = event['target']
+        setNewItem(target['value'])
+
+    return el('form',  {'onSubmit': handleSubmit},
+              el('label', {'htmlFor': 'newItem'}, "New Item: "),
+              el('input', {'id': 'newItem',
+                           'onChange': handleChange,
+                           'value': newItem
+                          }
+                ),
+              el('input',  {'type':  'submit'}),
+              el('ol', None,
+                  el(ListItems, {'items': items})
+                )
+             )
+
+
+render(App, None, 'root')
---
+++ sourcemap.py
@@ -0,0 +1,4 @@
+def print_stuff():
+    console.log("Native JS console.log call")
+    print("Python print")
+    console.invalid_method("This will be an error")
